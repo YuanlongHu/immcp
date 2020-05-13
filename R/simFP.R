b43ac37595560e1a643b2e2ec39a570d2cf7f89a
YuanlongHu <huyuanlong1996@163.com>
2020-05-13 20:13:59 +0800

    updata

---
+++ R/simFP.R
@@ -0,0 +1,39 @@
+#' Calculate the similarity between Drug Fingerprints
+#'
+#'
+#' @title simFP
+#' @param FP A ScoreFP object
+#' @return a matrix
+#' @importFrom Matrix Matrix
+#' @importFrom proxyC simil
+#' @export
+#' @author Yuanlong Hu
+
+
+
+simFP <- function(FP){
+ FP <- FP@Fingerprint[-1]
+ f1 <- as.data.frame(FP) %>%
+   t() %>%
+   Matrix::Matrix() %>%
+   proxyC::simil(method = "jaccard") %>%
+   as.matrix()
+
+ return(f1)
+}
+
+
+#' Visualization of a drug fingerprints similarity matrix using ggplot2
+#'
+#'
+#' @title Heatmap_simFP
+#' @param data A drug fingerprints similarity matrix
+#' @return a ggplot2
+#' @importFrom ggcorrplot ggcorrplot
+#' @export
+#' @author Yuanlong Hu
+#'
+Heatmap_simFP <- function(data, ...){
+  ggcorrplot(data, ...)
+}
+
