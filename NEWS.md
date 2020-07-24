cd544da830541d9d1e61f2ef590d7898c2f8a9a9
YuanlongHu <huyuanlong1996@163.com>
2020-07-24 17:12:21 +0800

    add getDEG function

--- NEWS.md
+++ NEWS.md
@@ -1,7 +1,7 @@
 CHANGES IN VERSION 0.9.1
 ------------------------
 
-* Add `CreateBasicData` function
+* Add `CreateBasicData` and `getDEG` function
 
 
 CHANGES IN VERSION 0.9.1
@@ -20,5 +20,5 @@ CHANGES IN VERSION 0.9.0
 CHANGES IN VERSION 0.8.4
 ------------------------
 
-* Add as.data.frame, head, tail methods for ScoreResult. <2020-06-10, Wed>
+* Add `as.data.frame`, `head`, `tail` methods for ScoreResult. <2020-06-10, Wed>
 * Add CRAN version.
--- R/extrFP.R
+++ R/extrFP.R
@@ -200,3 +200,44 @@ getF <- function(expr, pdata, level = "gene", withTentative = TRUE, geneset){
   message("Done...")
   return(features)
 }
+
+
+##' Calculate differentially expressed genes using limma method.
+##'
+##'
+##' @title getDEG
+##' @param data A matrix of expression values where rows correspond to genes and columns correspond to samples.
+##' @param pdata A character vector of phenotype.
+##' @param contrasts character vector specifying contrasts
+##' @return A list
+##' @importFrom stats model.matrix
+##' @importFrom limma makeContrasts
+##' @importFrom limma lmFit
+##' @importFrom limma contrasts.fit
+##' @importFrom limma eBayes
+##' @importFrom limma topTable
+##' @importFrom magrittr %>%
+##' @export
+##' @author Yuanlong Hu
+
+getDEG <- function(data, pdata, contrasts){
+  Group <- factor(pdata)
+  design <- model.matrix(~0 + Group)
+  colnames(design) <- unique(pdata)
+
+  # Construct Matrix of Custom Contrasts
+  contrast_matrix <- makeContrasts(contrasts = contrasts,
+                                   levels = design)
+  fit <- lmFit(data, design) %>%
+    contrasts.fit(contrast_matrix) %>%
+    eBayes()
+
+  res_DEG0 <- NULL
+  for (i in 1:length(contrasts)) {
+    res_DEG <-  topTable(fit, adjust.method = "fdr", number = Inf, coef = i) %>%
+      list()
+    names(res_DEG) <- contrasts[i]
+    res_DEG0 <- c(res_DEG0, res_DEG)
+  }
+  return(res_DEG0)
+}
